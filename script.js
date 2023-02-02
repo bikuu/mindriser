fa23014f1e980164c698d2308ac9af808c596ce2
bikuu <bikuu@github.com>
2023-02-02 20:12:27 +0545

    Finish the design of the main page.

--- script.js
+++ script.js
@@ -1,3 +1,32 @@
+// Count NUmbers of course years etc.
+window.addEventListener("scroll", function () {
+  let element = document.querySelector(".about-timetable");
+  let rect = element.getBoundingClientRect();
+  let isVisible = rect.top >= 0 && rect.bottom <= window.innerHeight;
+  if (isVisible) {
+    for (let i = 1; i <= 5; i++) {
+      setTimeout(() => {
+        document.getElementById("years").innerHTML = `${i}+`;
+      }, i * 10);
+    }
+    for (let i = 1; i <= 10; i++) {
+      setTimeout(() => {
+        document.getElementById("courses").innerHTML = `${i}+`;
+      }, i * 1);
+    }
+    for (let i = 1; i <= 5000; i++) {
+      setTimeout(() => {
+        document.getElementById("students").innerHTML = `${i}+`;
+      }, i * 0.1);
+    }
+    for (let i = 1; i <= 500; i++) {
+      setTimeout(() => {
+        document.getElementById("placements").innerHTML = `${i}+`;
+      }, i * 0.8);
+    }
+  }
+});
+
 // <!-- Initialize Swiper -->
 
 var swiper = new Swiper(".mySwiper", {
@@ -13,3 +42,47 @@ var swiper = new Swiper(".mySwiper", {
     clickable: true,
   },
 });
+
+// Testimonial Swiper
+
+var swiper = new Swiper(".mySwiper1", {
+  slidesPerView: "auto",
+  centeredSlides: true,
+  speed: 1000,
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+// Scroll to Top
+
+// function topFunction() {
+//   window.scroll({ top: 0, behavior: "smooth" });
+// }
+
+// Get the button:
+let myBtn = document.getElementById("myBtn");
+
+// When the user scrolls down 50px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    myBtn.style.display = "block";
+  } else {
+    myBtn.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  window.scroll({ top: 0, behavior: "smooth" });
+}
